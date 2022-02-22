04eb39fe1fb3e51d34ee9a42d0c336c27baf958b
KimSumin <ajou456@ajou.ac.kr>
2022-02-23 04:31:14 +0900

    Feat: Improve multiple web page
    using the query string.

--- main.js
+++ main.js
@@ -4,51 +4,31 @@ var fs = require('fs');
 
 var app = http.createServer(function(request, response){
     var _url = request.url;
-
-    var content = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - Welcome</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="index.html">WEB</a></h1>
-      <ol>
-        <li><a href="1.html">HTML</a></li>
-        <li><a href="2.html">CSS</a></li>
-        <li><a href="3.html">JavaScript</a></li>
-      </ol>
-      <h2>WEB</h2>
-      <p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
-      </p>
-    </body>
-    </html>
-    `;
+    var _query = url.parse(_url, true).query;
     
-    if (_url == '/index.html') {
-      fs.readFile('index.html', function(err, data){
-        response.writeHead(200);
-        response.end(data);
-      });
-    } else if (_url == '/1.html') {
-      fs.readFile('1.html', function(err, data){
-        response.writeHead(200);
-        response.end(data);
-      });
-    } else if (_url == '/2.html') {
-      fs.readFile('2.html', function(err, data){
-        response.writeHead(200);
-        response.end(data);
-      });
-    } else if (_url == '/3.html') {
-      fs.readFile('3.html', function(err, data){
-        response.writeHead(200);
-        response.end(data);
-      });
-    } else {
+    fs.readFile(`data/${_query.title}.html`, function(err, description){
+      var title = _query.title;
+      var layout = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>>${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/?title=index">WEB</a></h1>
+        <ol>
+          <li><a href="/?title=HTML">HTML</a></li>
+          <li><a href="/?title=CSS">CSS</a></li>
+          <li><a href="/?title=JS">JS</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
       response.writeHead(200);
-      return response.end(content);
-    }
+      response.end(layout);
+    });
 })
 app.listen(3000);
