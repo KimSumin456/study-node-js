c4572108bbfa32ffd1233c548ab6da7f80f23f35
KimSumin <ajou456@ajou.ac.kr>
2022-02-23 14:22:35 +0900

    Feat: Implement inputing command-line arguments

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,7 @@
+// 터미널에서 node helloworld.js 로 실행
 console.log("Hello world!");
 
-//터미널에서 node helloworld.js 로 실행
+// 터미널에서 node helloworld.js a b c 로 실행
+var args = process.argv;
+console.log(args);
+console.log(args[2] + args[3]);
