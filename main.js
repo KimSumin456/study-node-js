42835991d046e15911e815b82aec36f74202b3ca
KimSumin <ajou456@ajou.ac.kr>
2022-02-23 15:44:07 +0900

    Refactor: Change the layout codes to functions
    using function definition.

--- main.js
+++ main.js
@@ -2,6 +2,38 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+function getLayoutList(files) {
+  var list = ``;
+
+  for (var i = 0; i < files.length; i++) {
+    list += `<li><a href="/?title=${files[i]}">${files[i]}</a></li>`;
+  }
+
+  return list;
+}
+
+function getLayout(list, title, description) {
+  var layout = `
+  <!doctype html>
+  <html>
+  <head>
+    <title>${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/?title=WEB">WEB</a></h1>
+    <ol>
+      ${list}
+    </ol>
+    <h2>${title}</h2>
+    <p>${description}</p>
+  </body>
+  </html>
+  `;
+  
+  return layout;
+}
+
 var app = http.createServer(function(request, response){
     var _url = request.url;
     var _query = url.parse(_url, true).query;
@@ -9,10 +41,7 @@ var app = http.createServer(function(request, response){
     
     if (_pathname == '/') {
       fs.readdir('data/', function(error, files) {
-        var list = ``;
-        for (var i = 0; i < files.length; i++) {
-          list += `<li><a href="/?title=${files[i]}">${files[i]}</a></li>`;
-        }
+        var list = getLayoutList(files);
 
         fs.readFile(`data/${_query.title}`, function(error, description){
           var title = _query.title;
@@ -21,23 +50,8 @@ var app = http.createServer(function(request, response){
             description = 'Hello, Node.js!';
           }
   
-          var layout = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/?title=WEB">WEB</a></h1>
-            <ol>
-              ${list}
-            </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+          var layout = getLayout(list, title, description);
+
           response.writeHead(200);
           response.end(layout);
         });
@@ -46,6 +60,5 @@ var app = http.createServer(function(request, response){
       response.writeHead(404);
       response.end('Not found');
     }
-
 })
 app.listen(3000);
