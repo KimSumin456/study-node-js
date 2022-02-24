af4763eb96950e7abb2c62ff754aeedb646673ca
KimSumin <ajou456@ajou.ac.kr>
2022-02-24 22:49:56 +0900

    Refactor: Change the layout codes to object
    using object definition.

--- main.js
+++ main.js
@@ -3,45 +3,45 @@ var url = require('url');
 var fs = require('fs');
 var qs = require('querystring');
 
-function getLayoutList(files) {
-  var list = ``;
-
-  for (var i = 0; i < files.length; i++) {
-    list += `<li><a href="/?title=${files[i]}">${files[i]}</a></li>`;
-  }
-
-  return list;
-}
-
-function getLayout(list, title, description) {
-  var layout = `
-  <!doctype html>
-  <html>
-  <head>
-    <title>${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/?title=WEB">WEB</a></h1>
-    <ol>
-      ${list}
-    </ol>
-
-    <a href="/create">Create</a>
-    <a href="/update?title=${title}">Update</a>
-    <form action="/delete_process" method="post">
-      <input type="hidden" name="title" value="${title}">
-      <input type="submit" value="delete">
-    </form>
-
-    <h2>${title}</h2>
-    <p>${description}</p>
-  </body>
-  </html>
-  `;
+var layout = {
+  getHtml: function (list, title, description) {
+    return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/?title=WEB">WEB</a></h1>
+      <ol>
+        ${list}
+      </ol>
   
-  return layout;
-}
+      <a href="/create">Create</a>
+      <a href="/update?title=${title}">Update</a>
+      <form action="/delete_process" method="post">
+        <input type="hidden" name="title" value="${title}">
+        <input type="submit" value="delete">
+      </form>
+  
+      <h2>${title}</h2>
+      <p>${description}</p>
+    </body>
+    </html>
+    `;
+  },
+
+  getList: function (files) {
+    var list = ``;
+  
+    for (var i = 0; i < files.length; i++) {
+      list += `<li><a href="/?title=${files[i]}">${files[i]}</a></li>`;
+    }
+  
+    return list;
+  }
+};
 
 var app = http.createServer(function(request, response){
     var _url = request.url;
@@ -50,7 +50,7 @@ var app = http.createServer(function(request, response){
     
     if (_pathname == '/') {
       fs.readdir('data/', function(error, files) {
-        var list = getLayoutList(files);
+        var list = layout.getList(files);
 
         fs.readFile(`data/${_query.title}`, function(error, description){
           var title = _query.title;
@@ -59,15 +59,15 @@ var app = http.createServer(function(request, response){
             description = 'Hello, Node.js!';
           }
   
-          var layout = getLayout(list, title, description);
+          var html = layout.getHtml(list, title, description);
 
           response.writeHead(200);
-          response.end(layout);
+          response.end(html);
         });
       })
     } else if (_pathname == '/create') {      
       fs.readdir('data/', function(error, files){
-        var list = getLayoutList(files);
+        var list = layout.getList(files);
 
         var title = 'Create';
         var form = `
@@ -78,34 +78,30 @@ var app = http.createServer(function(request, response){
         </form>
         `;
 
-        var layout = getLayout(list, title, form);
+        var html = layout.getHtml(list, title, form);
 
         response.writeHead(200);
-        response.end(layout);
+        response.end(html);
       });
     } else if (_pathname == '/create_process') {
-      fs.readdir('data/', function(error, files){
-        var list = getLayoutList(files);
-
-        var data = '';
-        request.on('data', function(chunk){
-          data += chunk;        
-        });
-        request.on('end', function(){
-          var dataParsed = qs.parse(data);
-          var title = dataParsed.title;
-          var description = dataParsed.description;
-          
-          fs.writeFile(`data/${title}`, description, function(err){
-            if (err) throw err;
-            response.writeHead(302, { 'Location': `/?title=${title}` });
-            response.end();
-          });
+      var data = '';
+      request.on('data', function(chunk){
+        data += chunk;        
+      });
+      request.on('end', function(){
+        var dataParsed = qs.parse(data);
+        var title = dataParsed.title;
+        var description = dataParsed.description;
+        
+        fs.writeFile(`data/${title}`, description, function(err){
+          if (err) throw err;
+          response.writeHead(302, { 'Location': `/?title=${title}` });
+          response.end();
         });
       });
     } else if (_pathname == '/update') {      
       fs.readdir('data/', function(error, files){
-        var list = getLayoutList(files)
+        var list = layout.getList(files)
 
         fs.readFile(`data/${_query.title}`, function(error, description){
           var title = 'Update';
@@ -118,54 +114,46 @@ var app = http.createServer(function(request, response){
           </form>
           `;
 
-          var layout = getLayout(list, title, form);
+          var html = layout.getHtml(list, title, form);
 
           response.writeHead(200);
-          response.end(layout);
+          response.end(html);
         });
       });
     } else if (_pathname == '/update_process') {
-      fs.readdir('data/', function(error, files){
-        var list = getLayoutList(files);
-
-        var data = '';
-        request.on('data', function(chunk){
-          data += chunk;        
-        });
-        request.on('end', function(){
-          var dataParsed = qs.parse(data);
-          var titlePrev = dataParsed.title;
-          var title = dataParsed.title;
-          var description = dataParsed.description;
-          
-          fs.rename(`data/${titlePrev}`, `data/${title}`, function(err){
+      var data = '';
+      request.on('data', function(chunk){
+        data += chunk;        
+      });
+      request.on('end', function(){
+        var dataParsed = qs.parse(data);
+        var titlePrev = dataParsed.title;
+        var title = dataParsed.title;
+        var description = dataParsed.description;
+        
+        fs.rename(`data/${titlePrev}`, `data/${title}`, function(err){
+          if (err) throw err;
+          fs.writeFile(`data/${title}`, description, function(err){
             if (err) throw err;
-            fs.writeFile(`data/${title}`, description, function(err){
-              if (err) throw err;
-              response.writeHead(302, { 'Location': `/?title=${title}` });
-              response.end();
-            });
+            response.writeHead(302, { 'Location': `/?title=${title}` });
+            response.end();
           });
         });
       });
     } else if (_pathname == '/delete_process') {
-      fs.readdir('data/', function(error, files){
-        var list = getLayoutList(files);
+      var data = '';
+      request.on('data', function(chunk){
+        data += chunk;        
+      });
+      request.on('end', function(){
+        var dataParsed = qs.parse(data);
+        var title = dataParsed.title;
 
-        var data = '';
-        request.on('data', function(chunk){
-          data += chunk;        
-        });
-        request.on('end', function(){
-          var dataParsed = qs.parse(data);
-          var title = dataParsed.title;
-
-          fs.unlink(`data/${title}`, function(err){
-            if (err) throw err;
-            
-            response.writeHead(302, { 'Location': `/` });
-            response.end();
-          });
+        fs.unlink(`data/${title}`, function(err){
+          if (err) throw err;
+          
+          response.writeHead(302, { 'Location': `/` });
+          response.end();
         });
       });
     } else {
